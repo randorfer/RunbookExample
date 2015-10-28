f983678a7ce83646486ae8d7b9b2d05e1698f880
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-28 14:49:32 -0500

    update

--- Runbooks/FTP/New-FTPSetup-SharePoint.ps1
+++ Runbooks/FTP/New-FTPSetup-SharePoint.ps1
@@ -1,107 +1,114 @@
-Workflow New-FTPSetup-SharePoint
+
+Param(
+    [Parameter(
+        Mandatory = $True
+    )]
+    [string]
+    $NewRequestURI
+)
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName $WorkflowCommandName
+
+Try
 {
-    Param(
-        [Parameter(
-            Mandatory = $True,
-            ValueFromPipeLine = $True
-        )]
-        [string]
-        $NewRequestURI
-    )
+    $GlobalVars = Get-BatchAutomationVariable -Name 'SharePointCredName',
+                                                    'ExchangeCredName',
+                                                    'DomainCredName' `
+                                                -Prefix 'Global'
+        
+    $CommunicationVars = Get-BatchAutomationVariable -Name 'TemplateMailboxName',
+                                                            'TemplateFolder',
+                                                            'CredentialName' `
+                                                        -Prefix 'AutomationCommunication'
 
-    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
-    $CompletedParameters = Write-StartingMessage -CommandName $WorkflowCommandName
+    $Vars = Get-BatchAutomationVariable -Name 'UserOUPath',
+                                                'GroupOUPath',
+                                                'PasswordLength',
+                                                'PasswordSpecialCharacters',
+                                                'ErrorDistlist',
+                                                'SupportContact',
+                                                'ADDomain' `
+                                        -Prefix 'FTP'
+    $SharePointCredential = Get-AutomationPSCredential -Name $GlobalVars.SharePointCredName
+    $DomainCredential = Get-AutomationPSCredential -Name $GlobalVars.DomainCredName
+    $CommunicationCredential = Get-AutomationPSCredential -Name $CommunicationVars.CredentialName
 
-    Try
+
+    $Request = Get-SPListItem -SPUri $NewRequestURI -Credential $SharePointCredential -ExpandProperty 'GroupMembers', 'AccountOwner', 'CreatedBy'
+    $Requester = ConvertTo-PrimaryUserId -Identity $Request.LinkedItems.CreatedBy.Properties.Account -Properties mail
+    $AccountOwner = ConvertTo-PrimaryUserId -Identity $Request.LinkedItems.AccountOwner.Properties.Account -Properties mail
+        
+    $GroupMember = @()
+    Foreach($_GroupMember in $Request.LinkedItems.GroupMembers)
     {
-        $GlobalVars = Get-BatchAutomationVariable -Name 'SharePointCredName',
-                                                        'ExchangeCredName',
-                                                        'SecurityErrorDistlist',
-                                                        'SecuritySupportContact',
-                                                        'DomainCredName' `
-                                                  -Prefix 'Global'
-
-        $CommunicationVars = Get-BatchAutomationVariable -Name 'TemplateMailboxName',
-                                                               'TemplateFolder',
-                                                               'CredentialName' `
-                                                         -Prefix 'AutomationCommunication'
-        
-        $Vars = Get-BatchAutomationVariable -Name 'UserOUPath',
-                                                  'GroupOUPath',
-                                                  'PasswordLength',
-                                                  'PasswordSpecialCharacters',
-                                                  'ADDomain' `
-                                            -Prefix 'FTP'
-
-        $SharePointCredential = Get-AutomationPSCredential -Name $GlobalVars.SharePointCredName
-        $DomainCredential = Get-AutomationPSCredential -Name $GlobalVars.DomainCredName
-        $CommunicationCredential = Get-AutomationPSCredential -Name $CommunicationVars.CredentialName
-
-        $Request = Get-SPListItem -SPUri $NewRequestURI -Credential $SharePointCredential -ExpandProperty 'GroupMembers', 'AccountOwner', 'CreatedBy'
-        $Requester = ConvertTo-PrimaryUserId -Identity $Request.LinkedItems.CreatedBy.Properties.Account -Properties mail
-        $AccountOwner = ConvertTo-PrimaryUserId -Identity $Request.LinkedItems.AccountOwner.Properties.Account -Properties mail
-        
-        $GroupMember = @()
-        Foreach($_GroupMember in $Request.LinkedItems.GroupMembers)
+        Try
         {
-            Try
-            {
-                $GroupMember += ConvertTo-PrimaryUserId -Identity $_GroupMember.Properties.Account -Properties mail
-            }
-            Catch
-            {
-                Write-Exception -Exception $_ -Stream Warning
-            }
+            $GroupMember += ConvertTo-PrimaryUserId -Identity $_GroupMember.Properties.Account -Properties mail
         }
-        $RequesterEmail = $Requester.Mail
-
-        $FTPSetup = New-ADFTPSetup -Owner $AccountOwner `
-                                   -FTPUserPath $Vars.UserOUPath `
-                                   -FTPGroupPath $Vars.GroupOUPath `
-                                   -CompanyName $Request.Properties.CompanyName `
-                                   -PasswordLength $Vars.PasswordLength `
-                                   -PasswordNonAlphaNumbericCharacterCount $Vars.PasswordSpecialCharacters `
-                                   -Members $GroupMember `
-                                   -Server $Vars.ADDomain `
-                                   -Credential $DomainCredential
-
-        $AdditionalInformation  = "FTP User: $($FTPSetup.FTPUser.SamAccountName)<br/>"
-        $AdditionalInformation += "FTP User Password: $($FTPSetup.FTPUserPassword)<br/>"
-        $AdditionalInformation += "FTP Group: $($FTPSetup.FTPGroup.Name)<br/>"
-        Send-AutomationCommunication -RequestName 'New FTP Setup' `
-                                             -Type Success `
-                                             -RequestLink $NewRequestURI `
-                                             -AdditionalInformation $AdditionalInformation `
-                                             -To $RequesterEmail `
-                                             -Cc $GlobalVars.SecurityErrorDistlist `
-                                             -Contact $GlobalVars.SecuritySupportContact `
-                                             -TemplateMailboxName $CommunicationVars.TemplateMailboxName `
-                                             -TemplateFolder $CommunicationVars.TemplateFolder `
-                                             -Credential $CommunicationCredential
-    }
-    Catch
-    {
-        $Exception = $_
-        $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
-
-        Switch($ExceptionInfo.FullyQualifiedErrorId)
+        Catch
         {
-            Default
-            {
-                Write-Exception -Exception $Exception -Stream Warning
-                Send-AutomationCommunication -RequestName 'New FTP Setup' `
-                                             -Type Failure `
-                                             -RequestLink $NewRequestURI `
-                                             -AdditionalInformation $AdditionalInformation `
-                                             -To $RequesterEmail `
-                                             -Cc $GlobalVars.SecurityErrorDistlist `
-                                             -Contact $GlobalVars.SecuritySupportContact `
-                                             -TemplateMailboxName $CommunicationVars.TemplateMailboxName `
-                                             -TemplateFolder $CommunicationVars.TemplateFolder `
-                                             -Credential $CommunicationCredential
-            }
+            Write-Exception -Exception $_ -Stream Warning
         }
     }
+    $RequesterEmail = $Requester.Mail
 
-    Write-CompletedMessage -StartTime $CompletedParameters.StartTime -Name $CompletedParameters.Name -Stream $CompletedParameter.Stream
+    $FTPSetup = New-ADFTPSetup -Owner $AccountOwner.DistinguishedName `
+                                -FTPUserPath $Vars.UserOUPath `
+                                -FTPGroupPath $Vars.GroupOUPath `
+                                -CompanyName $Request.Properties.CompanyName `
+                                -PasswordLength $Vars.PasswordLength `
+                                -PasswordNonAlphaNumbericCharacterCount $Vars.PasswordSpecialCharacters `
+                                -Members $GroupMember.DistinguishedName `
+                                -Server $Vars.ADDomain `
+                                -Credential $DomainCredential
+
+    $AdditionalInformation  = "FTP User: $($FTPSetup.FTPUser.SamAccountName)<br/>"
+    $AdditionalInformation += "FTP User Password: $($FTPSetup.FTPUserPassword)<br/>"
+    $AdditionalInformation += "FTP Group: $($FTPSetup.FTPGroup.Name)<br/>"
+    Send-AutomationCommunication -RequestName 'New FTP Setup' `
+                                            -Type Success `
+                                            -RequestLink $Request.DisplayI `
+                                            -AdditionalInformation $AdditionalInformation `
+                                            -To $RequesterEmail `
+                                            -Cc $Vars.ErrorDistlist `
+                                            -Contact $Vars.SupportContact `
+                                            -TemplateMailboxName $CommunicationVars.TemplateMailboxName `
+                                            -TemplateFolder $CommunicationVars.TemplateFolder `
+                                            -Credential $CommunicationCredential
+
+    Update-SPListItem -SPUri $NewRequestURI `
+                        -Data @{ 'Status' = 'Success' } `
+                        -Credential $SharePointCredential
+    New-VariableRunbookTrackingInstance -VariablePrefix 'FTP-New'
 }
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+
+    Switch($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception -Exception $Exception -Stream Warning
+            $AdditionalInformation = Format-AdditionalInformation -Exception $Exception
+            Send-AutomationCommunication -RequestName 'New FTP Setup' `
+                                            -Type Failure `
+                                            -RequestLink $Request.DisplayId `
+                                            -AdditionalInformation $AdditionalInformation `
+                                            -To $RequesterEmail `
+                                            -Cc $Vars.ErrorDistlist `
+                                            -Contact $Vars.SupportContact `
+                                            -TemplateMailboxName $CommunicationVars.TemplateMailboxName `
+                                            -TemplateFolder $CommunicationVars.TemplateFolder `
+                                            -Credential $CommunicationCredential
+
+            Update-SPListItem -SPUri $NewRequestURI `
+                                -Data @{ 'Status' = 'Error' } `
+                                -Credential $SharePointCredential
+        }
+    }
+}
+
+Write-CompletedMessage -StartTime $CompletedParameters.StartTime -Name $CompletedParameters.Name -Stream $CompletedParameters.Stream
