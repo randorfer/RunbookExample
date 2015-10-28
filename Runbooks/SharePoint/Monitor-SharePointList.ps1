6b49559605c9670d90b8812c3a289afeaf991466
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-10-28 11:26:55 -0500

    migrate to ps script and use start-sleepuntil

--- Runbooks/SharePoint/Monitor-SharePointList.ps1
+++ Runbooks/SharePoint/Monitor-SharePointList.ps1
@@ -2,58 +2,50 @@
     .Synopsis
         Monitors sharepoint lists for jobs to start
 #>
-Workflow Monitor-SharePointList
+Param (
+)
+
+$Vars = Get-BatchAutomationVariable -Name 'SourceSPFarm',
+                                            'SourceSPSite',
+                                            'SourceSPList',
+                                            'SourceSPCollection',
+                                            'SourceSPStatusField',
+                                            'LastResultSPField',
+                                            'SourceSPStartValue',
+                                            'DelayCycle',
+                                            'DelayCheckpoint',
+                                            'DefaultSPActionCredName',
+                                            'AutomationAccountName',
+                                            'SubscriptionName',
+                                            'ResourceGroupName',
+                                            'AzureAutomationAccessCredentialName',
+                                            'SMTPServer',
+                                            'From',
+                                            'RunOn' `
+                                    -Prefix 'MonitorSharePoint'
+do
 {
-    Param (
-    )
-
-    $Vars = Get-BatchAutomationVariable -Name 'SourceSPFarm',
-                                              'SourceSPSite',
-                                              'SourceSPList',
-                                              'SourceSPCollection',
-                                              'SourceSPStatusField',
-                                              'LastResultSPField',
-                                              'SourceSPStartValue',
-                                              'DelayCycle',
-                                              'DelayCheckpoint',
-                                              'DefaultSPActionCredName',
-                                              'AutomationAccountName',
-                                              'SubscriptionName',
-                                              'ResourceGroupName',
-                                              'AzureAutomationAccessCredentialName',
-                                              'SMTPServer',
-                                              'From',
-                                              'RunOn' `
-                                        -Prefix 'MonitorSharePoint'
-    do
-    {
-        $NextRun = (Get-Date).AddSeconds(30)
+    $NextRun = (Get-Date).AddSeconds(30)
         
-        $AzureAutomationCredential = Get-AutomationPSCredential -Name $Vars.AzureAutomationAccessCredentialName
-        $SharePointCredential = Get-AutomationPSCredential -Name $Vars.DefaultSPActionCredName
+    $AzureAutomationCredential = Get-AutomationPSCredential -Name $Vars.AzureAutomationAccessCredentialName
+    $SharePointCredential = Get-AutomationPSCredential -Name $Vars.DefaultSPActionCredName
 
-        Invoke-SharePointRunbookJob -SharePointCredential $SharePointCredential `
-                                    -Farm $Vars.SourceSPFarm `
-                                    -Site $Vars.SourceSPSite `
-                                    -Collection $Vars.SourceSPCollection `
-                                    -List $Vars.SourceSPList `
-                                    -StatusField $Vars.SourceSPStatusField `
-                                    -EnvironmentValue $Vars.SourceSPStartValue `
-                                    -AzureAutomationCredential $AzureAutomationCredential `
-                                    -SubscriptionName $Vars.SubscriptionName `
-                                    -ResourceGroupName $Vars.ResourceGroupName `
-                                    -AutomationAccountName $Vars.AutomationAccountName `
-                                    -SMTPServer $Vars.SMTPServer `
-                                    -From $Vars.From `
-                                    -LastResultSPField $Vars.LastResultSPField `
-                                    -RunOn $Vars.RunOn
+    Invoke-SharePointRunbookJob -SharePointCredential $SharePointCredential `
+                                -Farm $Vars.SourceSPFarm `
+                                -Site $Vars.SourceSPSite `
+                                -Collection $Vars.SourceSPCollection `
+                                -List $Vars.SourceSPList `
+                                -StatusField $Vars.SourceSPStatusField `
+                                -EnvironmentValue $Vars.SourceSPStartValue `
+                                -AzureAutomationCredential $AzureAutomationCredential `
+                                -SubscriptionName $Vars.SubscriptionName `
+                                -ResourceGroupName $Vars.ResourceGroupName `
+                                -AutomationAccountName $Vars.AutomationAccountName `
+                                -SMTPServer $Vars.SMTPServer `
+                                -From $Vars.From `
+                                -LastResultSPField $Vars.LastResultSPField `
+                                -RunOn $Vars.RunOn
         
-        do
-        {
-            Start-Sleep -Seconds 5
-            Checkpoint-Workflow
-            $Sleeping = (Get-Date) -lt $NextRun
-        } while($Sleeping)
-    }
-    while($true)
+    Start-SleepUntil -DateTime $NextRun
 }
+while($true)
