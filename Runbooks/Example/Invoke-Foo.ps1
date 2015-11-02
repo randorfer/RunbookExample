d6a19442f7072008fa22ca389e7f468f3ce50269
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-11-02 14:48:40 -0600

    updates

--- Runbooks/Example/Invoke-Foo.ps1
+++ Runbooks/Example/Invoke-Foo.ps1
@@ -1,1 +1,14 @@
-Write-Verbose 'Hello World'
+Workflow Invoke-Foo
+{
+    Param(
+    )
+    $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    $CompletedParams = Write-StartingMessage -CommandName $WorkflowCommandName
+
+    $Vars = Get-BatchAutomationVariable -Prefix 'Foo' -Name @('a','b','credname')
+    $Credential = Get-AutomationPSCredential -Name $Vars.credname
+
+    Write-Verbose 'Hello World'
+
+    Write-CompletedMessage -StartTime $CompletedParams.StartTime -Name $CompletedParams.Name -Stream $CompletedParams.Stream
+}
