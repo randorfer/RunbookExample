922241de81baa24fe985b3b5545daf5621329ff4
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-11-05 12:17:48 -0600

    update

--- Runbooks/Example/Invoke-Foo.ps1
+++ Runbooks/Example/Invoke-Foo.ps1
@@ -8,7 +8,7 @@ Workflow Invoke-Foo
     $Vars = Get-BatchAutomationVariable -Prefix 'Foo' -Name @('a','b','credname')
     $Credential = Get-AutomationPSCredential -Name $Vars.credname
 
-    Write-Verbose 'Hello World from the MVP summit'
+    Write-Verbose 'Hello World from the MVP summit!'
 
     Write-CompletedMessage -StartTime $CompletedParams.StartTime -Name $CompletedParams.Name -Stream $CompletedParams.Stream
 }
