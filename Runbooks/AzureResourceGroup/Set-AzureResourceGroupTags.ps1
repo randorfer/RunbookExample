cd84a4878f4c161da729a7ff71f2f16cc22264c8
Ryan Andorfer <ryan.andorfer@gmail.com>
2015-11-12 15:24:22 -0600

    update

--- Runbooks/AzureResourceGroup/Set-AzureResourceGroupTags.ps1
+++ Runbooks/AzureResourceGroup/Set-AzureResourceGroupTags.ps1
@@ -23,45 +23,46 @@ Try
         $Resource = Get-AzureRmResource -ResourceGroupName $_ResourceGroup.ResourceGroupName
         Foreach($_Resource in $Resource)
         {
-            if($_Resource.Tags -as [bool])
+            $Update = $false
+            Foreach($ResourceGroupTag in $_ResourceGroup.Tags)
             {
-                $Update = $false
-                Foreach($ResourceGroupTag in $_ResourceGroup.Tags)
+                $MatchFound = $False
+                Foreach($ResourceTag in $_Resource.Tags)
                 {
-                    $MatchFound = $False
-                    Foreach($ResourceTag in $_Resource.Tags)
+                    if($ResourceTag.Name -eq $ResourceGroupTag.Name)
                     {
-                        if($ResourceTag.Name -eq $ResourceGroupTag.Name)
+                        if($ResourceTag.Value -ne $ResourceGroupTag.Value)
                         {
-                            if($ResourceTag.Value -ne $ResourceGroupTag.Value)
-                            {
-                                $MatchFound = $True
-                                $Update = $True
-                                $ResourceTag.Value = $ResourceGroupTag.Value
-                            }
-                            else
-                            {
-                                $MatchFound = $True
-                                $Update = $True
-                            }
+                            $MatchFound = $True
+                            $Update = $True
+                            $ResourceTag.Value = $ResourceGroupTag.Value
+                        }
+                        else
+                        {
+                            $MatchFound = $True
+                            $Update = $True
                         }
                     }
-                    if($MatchFound -eq $false)
-                    {
-                        $Update = $True
-                        $Null = $_Resource.Tags.Add($ResourceGroupTag)
-                    }
                 }
-                if($Update)
+                if($MatchFound -eq $false)
                 {
-                    Try
+                    try
                     {
-                        $Null = $_Resource | Set-AzureRmResource -Force
+                    $Update = $True
+                    $Null = $_Resource.Tags += $ResourceGroupTag
                     }
-                    Catch
-                    {
-                        Write-Exception -Exception $_ -Stream Warning
-                    }
+                    catch { $Update = $false }
+                }
+            }
+            if($Update)
+            {
+                Try
+                {
+                    $Null = $_Resource | Set-AzureRmResource -Force
+                }
+                Catch
+                {
+                    Write-Exception -Exception $_ -Stream Warning
                 }
             }
         }
