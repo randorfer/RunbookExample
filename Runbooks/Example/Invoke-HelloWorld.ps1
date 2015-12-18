98db66952d476cb1343b9f12316c22015f54c9a1
Ryan.Andorfer <Ryan.Andorfer@microsoft.com>
2015-12-18 11:30:55 -0600

    demo

---
+++ Runbooks/Example/Invoke-HelloWorld.ps1
@@ -0,0 +1,10 @@
+﻿Param(
+    [String]
+    $Message
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParams = Write-StartingMessage -CommandName 'Invoke-HelloWorld'
+
+Write-Verbose -Message $Message
+
+Write-CompletedMessage @CompletedParams
