62fba71069c384bfa80b6b534e7125774a3b87fe
Ryan Andorfer <ryanand@microsoft.com>
2015-12-20 21:22:07 -0600

    test push

--- Runbooks/Example/Invoke-HelloWorld.ps1
+++ Runbooks/Example/Invoke-HelloWorld.ps1
@@ -6,6 +6,6 @@ $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $CompletedParams = Write-StartingMessage -CommandName 'Invoke-HelloWorld'
 
 
-Write-Verbose -Message "A cooler $($Message)"
+Write-Verbose -Message "A cooler $($Message)!"
 
 Write-CompletedMessage @CompletedParams
