4db45bdae944962525786fbec3215a2f8d2c7ca2
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2015-12-30 17:05:12 -0600

    update webserver

--- DSC/DemoWebserver.ps1
+++ DSC/DemoWebserver.ps1
@@ -9,7 +9,7 @@ Configuration DemoWebserver
     $Vars = Get-BatchAutomationVariable -Prefix 'DemoWebServer' -Name 'NuGetCredentialName'
     $NuGetCredential = Get-AutomationPSCredential -Name $Vars.NuGetCredentialName
 
-    Node localhost {   
+    Node FrontEnd {   
 
         #register package source       
         NuGetPackageRepository SourceRepository
@@ -49,5 +49,89 @@ Configuration DemoWebserver
             DependsOn       = "[NuGetPackageRepository]SourceRepository"
         }
         #>
-    }    
+    }
+
+    Node MiddleTier {   
+
+        #register package source       
+        NuGetPackageRepository SourceRepository
+        {
+            Ensure      = "Present"
+            Name        = "Application"
+            Source      = "https://scorchdev.pkgs.visualstudio.com/DefaultCollection/_packaging/Application/nuget/v2"  
+            Credential  = $NuGetCredential 
+        }   
+
+        WindowsFeature installIIS 
+        { 
+            Ensure="Present" 
+            Name="Web-Server" 
+        }
+
+        xFirewall WebFirewallRule 
+        { 
+            Direction = "Inbound" 
+            Name = "Web-Server-TCP-In" 
+            DisplayName = "Web Server (TCP-In)" 
+            Description = "IIS allow incoming web site traffic."
+            Action = "Allow"
+            Enabled = "True"
+            Protocol = "TCP" 
+            LocalPort = "80" 
+            Ensure = "Present" 
+        } 
+        <#
+        #Install a package from Nuget repository
+        NugetPackage Nuget
+        {
+            Ensure          = "Present" 
+            Name            = $Name
+            DestinationPath = $DestinationPath
+            RequiredVersion = "2.0.1"
+            DependsOn       = "[NuGetPackageRepository]SourceRepository"
+        }
+        #>
+    }
+
+    Node Database {   
+
+        #register package source       
+        NuGetPackageRepository SourceRepository
+        {
+            Ensure      = "Present"
+            Name        = "Application"
+            Source      = "https://scorchdev.pkgs.visualstudio.com/DefaultCollection/_packaging/Application/nuget/v2"  
+            Credential  = $NuGetCredential 
+        }   
+
+        WindowsFeature installIIS 
+        { 
+            Ensure="Present" 
+            Name="Web-Server" 
+        }
+
+        xFirewall WebFirewallRule 
+        { 
+            Direction = "Inbound" 
+            Name = "Web-Server-TCP-In" 
+            DisplayName = "Web Server (TCP-In)" 
+            Description = "IIS allow incoming web site traffic."
+            Action = "Allow"
+            Enabled = "True"
+            Protocol = "TCP" 
+            LocalPort = "80" 
+            Ensure = "Present" 
+        } 
+        <#
+        #Install a package from Nuget repository
+        NugetPackage Nuget
+        {
+            Ensure          = "Present" 
+            Name            = $Name
+            DestinationPath = $DestinationPath
+            RequiredVersion = "2.0.1"
+            DependsOn       = "[NuGetPackageRepository]SourceRepository"
+        }
+        #>
+    }
 }
