e235841a4336b1daa9b2aece4c67755e39210e8c
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-01-08 11:08:33 -0600

    update invoke wrapper to use PS Module

--- Runbooks/VirtualMachine/Invoke-VirtualMachineClone.ps1
+++ Runbooks/VirtualMachine/Invoke-VirtualMachineClone.ps1
@@ -32,98 +32,16 @@ $BackupSubscriptionAccessCredential = Get-AutomationPSCredential -Name $BackupAz
 
 Try
 {
-    $PrimaryAzureSubscriptionConnection = @{
-        'Credential' = $PrimarySubscriptionAccessCredential
-        'SubscriptionName' = $PrimaryAzureSubscriptionVars.Name
-        'Tenant' = $PrimaryAzureSubscriptionVars.Tenant
-    }
-
-    $BackupAzureSubscriptionConnection = @{
-        'Credential' = $BackupSubscriptionAccessCredential
-        'SubscriptionName' = $BackupAzureSubscriptionVars.Name
-        'Tenant' = $BackupAzureSubscriptionVars.Tenant
-    }
-
-    Connect-AzureRmAccount @BackupAzureSubscriptionConnection
-    $TargetStorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $CloneVars.ResourceGroup -Name $CloneVars.TargetStorageAccountName
-
-    Connect-AzureRmAccount @PrimaryAzureSubscriptionConnection
-    $VM = Get-AzureRmVM -ResourceGroupName $CloneVars.ResourceGroup
-
-    $DiskToCopy = New-Object -TypeName System.Collections.ArrayList
-    Foreach($_VM in $VM)
-    {
-        if($_VM.StorageProfile.OSDisk.VirtualHardDisk.Uri -Match '/([^/.]+).+/([^/]+)/([^/]+)$')
-        {
-            $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $CloneVars.ResourceGroup -Name $Matches[1]
-            $Null = $DiskToCopy.Add(@{ 'Context' = $StorageAccount.Context ; 'Container' = $Matches[2] ; 'BlobName' = $Matches[3] })
-        }
-        Foreach($DataDisk in $_vm.StorageProfile.DataDisks)
-        {
-            if($DataDisk.VirtualHardDisk.Uri -Match '/([^/.]+).+/([^/]+)/([^/]+)$')
-            {
-                $StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $CloneVars.ResourceGroup -Name $Matches[1]
-                $Null = $DiskToCopy.Add(@{ 'Context' = $StorageAccount.Context ; 'Container' = $Matches[2] ; 'BlobName' = $Matches[3] })
-            }
-        }
-    }
-
-    $BlobCopyJob = New-Object -TypeName System.Collections.ArrayList
-    Foreach($Disk in $DiskToCopy)
-    {
-        $DiskCopyStartCompletedParams = Write-StartingMessage -CommandName ($Disk.BlobName)
-        Try
-        {
-            Try
-            {
-                $Container = Get-AzureStorageContainer -Context $TargetStorageAccount.Context -Name $Disk.Container
-                if(-not($Container -as [bool]))
-                {
-                    $Null = New-AzureStorageContainer -Context $TargetStorageAccount.Context -Name $Disk.Container
-                }
-            }
-            Catch
-            {
-                $Null = New-AzureStorageContainer -Context $TargetStorageAccount.Context -Name $Disk.Container
-            }
-
-            $Null = $BlobCopyJob.Add(
-                (
-                    Start-CopyAzureStorageBlob -SrcBlob $Disk.BlobName `
-                                               -SrcContainer $Disk.Container `
-                                               -Context $Disk.Context `
-                                               -DestContainer $Disk.Container `
-                                               -DestBlob $Disk.BlobName `
-                                               -DestContext $TargetStorageAccount.Context `
-                                               -Force
-                )
-            )
-        }
-        Catch
-        {
-            Write-Exception -Exception $_ -Stream Warning
-        }
-        Write-CompletedMessage @DiskCopyStartCompletedParams
-    }
-
-    $NotComplete = $False
-    Do
-    {
-        Foreach($_BlobCopyJob in $BlobCopyJob)
-        {
-            if($_BlobCopyJob -as [bool])
-            {
-                $BlobCopyCompletedParams = Write-StartingMessage -CommandName $_BlobCopyJob.Name
-                $CopyState = $_BlobCopyJob | Get-AzureStorageBlobCopyState
-	        
-		        $Percent = ($copyState.BytesCopied / $copyState.TotalBytes) * 100		
-		        if($CopyState.Status -ne 'Success') { $NotComplete = $True }
-                Write-CompletedMessage @BlobCopyCompletedParams -Status "$($CopyState.Status) Completed $('{0:N2}' -f $Percent)%"
-            }
-        }
-        if($NotComplete) { Start-Sleep -Seconds 30 }
-    } While($NotComplete)
-
+    Copy-AzureRMResourceGroupVMDisk -SourceSubscriptionName $PrimaryAzureSubscriptionVars.Name `
+                                    -SourceSubscriptionAccessCredential $PrimarySubscriptionAccessCredential `
+                                    -SourceSubscriptionTenant $PrimaryAzureSubscriptionVars.Tenant `
+                                    -TargetSubscriptionName $BackupAzureSubscriptionVars.Name `
+                                    -TargetSubscriptionAccessCredential $BackupSubscriptionAccessCredential `
+                                    -TargetSubscriptionTenant $BackupAzureSubscriptionVars.Tenant `
+                                    -SourceResourceGroupName $CloneVars.ResourceGroup `
+                                    -TargetResourceGroupName $CloneVars.ResourceGroup `
+                                    -TargetStorageAccountName $CloneVars.TargetStorageAccountName `
+                                    -SourceStorageAccountName $CloneVars.SourceStorageAccountName
 }
 Catch
 {
