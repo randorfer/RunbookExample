c08e7c2cad2f0d309a0bcc8810c9239f4030730f
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-01-13 15:56:50 -0600

    update demowebserver config to include dom join and share map

--- DSC/DemoWebserver.ps1
+++ DSC/DemoWebserver.ps1
@@ -4,10 +4,19 @@ Configuration DemoWebserver
     )
 
     Import-DscResource -ModuleName xNetworking
-    Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+    Import-DscResource -ModuleName cNetworkShare
+    Import-DscResource -ModuleName xComputerManagement
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
 
     $Vars = Get-BatchAutomationVariable -Prefix 'DemoWebServer' -Name 'NuGetCredentialName'
     $NuGetCredential = Get-AutomationPSCredential -Name $Vars.NuGetCredentialName
+
+    $DomainComputerVars = Get-BatchAutomationVariable -Prefix 'DomainComputer' `
+                                        -Name 'DomainJoinCredName',
+                                              'PackagesNetworkShareCredName'
+    
+    $DomainJoinCred = Get-AutomationPSCredential -Name $Vars.DomainJoinCredName
+    $PackagesNetworkShareCred = Get-AutomationPSCredential -Name $Vars.DomainJoinCredName
 
     Node localhost {   
 
@@ -28,6 +37,21 @@ Configuration DemoWebserver
             Protocol = "TCP" 
             LocalPort = "80" 
             Ensure = "Present"
+        }
+
+        xComputer DomainComputer
+        {
+            Name = $Env:COMPUTERNAME
+            DomainName = 'SCOrchDev.com'
+            Credential = $DomainJoinCred
+        }
+
+        cNetworkShare PackagesNetworkShare
+        {
+            Ensure = 'Present'
+            DriveLetter = 'P'
+            Credential = $PackagesNetworkShareCred
+            SharePath = '\\scodev.file.core.windows.net\packages'
         }
     }
 }
