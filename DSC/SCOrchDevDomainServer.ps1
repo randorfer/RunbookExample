7d9c63c0b98426b4259b789af3bbc67296052521
Ryan Andorfer <Ryan.Andorfer@Microsoft.com>
2016-01-14 09:21:50 -0600

    Update to install the MMA Agent

--- DSC/SCOrchDevDomainServer.ps1
+++ DSC/SCOrchDevDomainServer.ps1
@@ -5,21 +5,29 @@ Configuration SCOrchDevDomainServer
 
     Import-DscResource -ModuleName cNetworkShare
     Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
 
     $Vars = Get-BatchAutomationVariable -Prefix 'DomainComputer' `
                                         -Name 'DomainJoinCredName',
-                                              'PackagesNetworkShareCredName'
+                                              'OIPackageLocalPath'
     
     $DomainJoinCred = Get-AutomationPSCredential -Name $Vars.DomainJoinCredName
     $PackagesNetworkShareCred = Get-AutomationPSCredential -Name $Vars.PackagesNetworkShareCredName
     
     Node Default {   
-        cNetworkShare PackagesNetworkShare
-        {
-            Ensure = 'Present'
-            DriveLetter = 'P'
-            Credential = $PackagesNetworkShareCred
-            SharePath = '\\scodev.file.core.windows.net\packages'
+        xRemoteFile OIPackage {
+            Uri = "https://opsinsight.blob.core.windows.net/publicfiles/MMASetup-AMD64.exe"
+            DestinationPath = $Vars.OIPackageLocalPath
+        }
+
+        #Application
+        Package OI {
+            Ensure = "Present"
+            Path  = $Vars.OIPackageLocalPath
+            Name = "Microsoft Monitoring Agent"
+            ProductId = ""
+            Arguments = "/C:`"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=`"$($Vars.OIWorkspaceId)`" OPINSIGHTS_WORKSPACE_KEY=`"$($Vars.OIWorkspaceKey)`""
+            DependsOn = "[xRemoteFile]OIPackage"
         }
     }
 }
