95c41b83429aea011451bc966d9d6188fc1355e0
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-02-24 08:24:22 -0600

    updates

--- Runbooks/PSCredential/Invoke-PSCredentialPasswordChange.ps1
+++ Runbooks/PSCredential/Invoke-PSCredentialPasswordChange.ps1
@@ -7,71 +7,19 @@ Param(
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $CompletedParameters = Write-StartingMessage
 
-$Vars = Get-BatchAutomationVariable -Name  'CredentialName',
-                                           'SubscriptionName',
+$Vars = Get-BatchAutomationVariable -Name  'SubscriptionAccessCredentialName',
                                            'SubscriptionName',
                                            'AutomationAccountName',
                                            'ResourceGroupName',
-                                           '' `
+                                           'Tenant' `
                                     -Prefix 'PSCredentialPasswordChange'
-Set-AutomationVariable -Prefix 'PSCredentialPasswordChange' -Name 'AutomationAccountName' -Value 'SCOrchDev-Staging'
-Set-AutomationVariable -Prefix 'PSCredentialPasswordChange' -Name 'ResourceGroupName' -Value 'SCOrchDev'
 
-$Credential = Get-AutomationPSCredential -Name $Vars.CredentialName
+$Credential = Get-AutomationPSCredential -Name $Vars.SubscriptionAccessCredentialName
 
 Try
 {
-    Connect-AzureRmAccount -Credential $Credential -SubscriptionName $Vars.SubscriptionName
-    $ResourceGroup = Get-AzureRmResourceGroup
-    Foreach($_ResourceGroup in $ResourceGroup)
-    {
-        $Resource = Get-AzureRmResource -ResourceGroupName $_ResourceGroup.ResourceGroupName
-        Foreach($_Resource in $Resource)
-        {
-            $Update = $false
-            Foreach($ResourceGroupTag in $_ResourceGroup.Tags)
-            {
-                $MatchFound = $False
-                Foreach($ResourceTag in $_Resource.Tags)
-                {
-                    if($ResourceTag.Name -eq $ResourceGroupTag.Name)
-                    {
-                        if($ResourceTag.Value -ne $ResourceGroupTag.Value)
-                        {
-                            $MatchFound = $True
-                            $Update = $True
-                            $ResourceTag.Value = $ResourceGroupTag.Value
-                        }
-                        else
-                        {
-                            $MatchFound = $True
-                            $Update = $True
-                        }
-                    }
-                }
-                if($MatchFound -eq $false)
-                {
-                    try
-                    {
-                    $Update = $True
-                    $Null = $_Resource.Tags += $ResourceGroupTag
-                    }
-                    catch { $Update = $false }
-                }
-            }
-            if($Update)
-            {
-                Try
-                {
-                    $Null = $_Resource | Set-AzureRmResource -Force
-                }
-                Catch
-                {
-                    Write-Exception -Exception $_ -Stream Warning
-                }
-            }
-        }
-    }
+    Connect-AzureRmAccount -Credential $Credential -SubscriptionName $Vars.SubscriptionName -Tenant $Vars.Tenant
+    
 }
 Catch
 {
