b57d519543a24e18f0b2394232351b5f80fe50da
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-02-25 18:24:12 -0600

    create invoke-testnetconnection

---
+++ Runbooks/Scratch/Invoke-TestNetConnection.ps1
@@ -0,0 +1,32 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-TestNetConnection
+
+Try
+{
+    Test-NetConnection -ComputerName 'www.google.com'
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
