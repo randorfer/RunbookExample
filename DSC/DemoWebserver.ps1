360328d1e959526a3b1eee1d5b6a81d7da56d9ff
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-02-25 18:20:51 -0600

    v1 3 tier app

--- DSC/DemoWebserver.ps1
+++ DSC/DemoWebserver.ps1
@@ -1,4 +1,4 @@
-Configuration DemoWebserver
+ Configuration DemoWebserver
 {
     Param(
     )
---
+++ DSC/EnterpriseApplication.ps1
@@ -0,0 +1,49 @@
+﻿ Configuration EnterpriseApplication
+{
+    Param(
+    )
+
+    Import-DscResource -ModuleName xNetworking
+    Import-DscResource -ModuleName cNetworkShare
+    Import-DscResource -ModuleName xComputerManagement
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -Module PackageManagementProviderResource
+    Import-DscResource -ModuleName xSQLServer
+    
+    Node FrontEndWebserver {   
+
+        WindowsFeature installIIS 
+        { 
+            Ensure="Present" 
+            Name="Web-Server" 
+        }
+
+        xFirewall WebFirewallRule 
+        { 
+            Direction = "Inbound" 
+            Name = "Web-Server-TCP-In" 
+            DisplayName = "Web Server (TCP-In)" 
+            Description = "IIS allow incoming web site traffic."
+            Action = "Allow"
+            Enabled = "True"
+            Protocol = "TCP" 
+            LocalPort = "80" 
+            Ensure = "Present"
+        }
+    }
+
+    Node SQLServer {
+        xFirewall WebFirewallRule 
+        { 
+            Direction = "Inbound" 
+            Name = "Web-Server-TCP-In" 
+            DisplayName = "Web Server (TCP-In)" 
+            Description = "IIS allow incoming web site traffic."
+            Action = "Allow"
+            Enabled = "True"
+            Protocol = "TCP" 
+            LocalPort = "80" 
+            Ensure = "Present"
+        }
+    }
+}
