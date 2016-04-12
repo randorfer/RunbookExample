cb97982c6e6c36b4928f737ce258817f5f07d7ff
Ryan Andorfer <Ryan.Andorfer@gmail.com>
2016-04-12 15:09:25 -0500

    Initial DSC for AzureAutomation/HybridRunbookWorker

---
+++ DSC/AzureAutomation.ps1
@@ -0,0 +1,128 @@
+﻿Configuration AzureAutomation
+{
+    Param(
+    )
+
+    #Import the required DSC Resources
+    Import-DscResource -Module xNetworking
+    Import-DscResource -Module xPSDesiredStateConfiguration
+    Import-DscResource -Module cChoco
+    
+    $MMAAgentRemoteURI = 'https://go.microsoft.com/fwlink/?LinkID=517476'
+    $MMASetupExe = 'MMASetup-AMD64.exe'
+    $SourceDir = 'c:\Sources'
+
+    $Vars = Get-BatchAutomationVariable -Prefix 'AzureAutomation' -Name @(
+        'WorkspaceID',
+        'AutomationAccountURL',
+        'AutomationAccountPrimaryKeyName',
+        'HybridRunbookWorkerGroupName'
+    )
+
+    $WorkspaceCredential = Get-AutomationPSCredential -Name $Vars.WorkspaceID
+    $WorkspaceKey = $WorkspaceCredential.GetNetworkCredential().Password
+
+    $PrimaryKeyCredential = Get-AutomationPSCredential -Name $Vars.AutomationAccountPrimaryKeyName
+    $PrimaryKey = $PrimaryKeyCredential.GetNetworkCredential().Password
+
+    $CommandLineArguments = 
+        "/Q /C:`"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 AcceptEndUserLicenseAgreement=1 " +
+        "OPINSIGHTS_WORKSPACE_ID=$($Vars.WorkspaceID) " +
+        "OPINSIGHTS_WORKSPACE_KEY=$($WorkspaceKey)`""
+    
+    $HybridRunbookWorkerConfiguredFlagFileName = 'hybridrunbookworkerconfigured'
+    Node HybridRunbookWorker
+    {
+        cChocoInstaller installChoco
+        {
+            InstallDir = $SourceDir
+        }
+        cChocoPackageInstaller installGit
+        {
+            Name = "git"
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+        xRemoteFile DownloadMicrosoftManagementAgent
+        {
+            Uri = $MMAAgentRemoteURI
+            DestinationPath = "$($SourceDir)\$($MMASetupExe)"
+            MatchSource = $False
+        }
+        Package InstalMicrosoftManagementAgent
+        {
+             Name = 'Microsoft Monitoring Agent' 
+             ProductId = 'E854571C-3C01-4128-99B8-52512F44E5E9'
+             Path = "$($SourceDir)\$($MMASetupExE)" 
+             Arguments = $CommandLineArguments 
+             Ensure = 'Present'
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+        Script RegisterHybridRunbookWorker
+        {
+            GetScript = {
+                if(Test-Path -Path 'HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker')
+                {
+                    Return (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker' -Name 'RunbookWorkerGroup').RunbookWorkerGroup
+                }
+                Return 'Not Configured'
+            }
+
+            SetScript = {
+                $StartingDir = (pwd).Path
+                Try
+                {
+                    cd "C:\Program Files\Microsoft Monitoring Agent\Agent\AzureAutomation"
+                    cd $((Get-ChildItem)[0].Name)
+                    cd HybridRegistration
+                    Import-Module .\HybridRegistration.psd1
+
+                    if((GetScript) -ne 'Not Configured')
+                    {
+                        Remove-HybridRunbookWorker -Url $Vars.AutomationAccountURL -Key $PrimaryKey
+                    }
+
+                    Add-HybridRunbookWorker -Url $Vars.AutomationAccountURL -Key $PrimaryKey -GroupName $Vars.HybridRunbookWorkerGroupName
+                }
+                Catch { throw }
+                Finally { Set-Location -Path $StartingDir }
+            }
+
+            TestScript = {
+                (GetScript) -eq $Vars.HybridRunbookWorkerGroupName
+            }
+        }
+        xFireWall OMS_HTTPS_Access
+        {
+            Direction = 'Outbound'
+            Name = 'HybridWorker-HTTPS'
+            DisplayName = 'Hybrid Runbook Worker (HTTPS)'
+            Description = 'Allow Hybrid Runbook Worker Communication'
+            Enabled = $true
+            Action = 'Allow'
+            Protocol = 'TCP'
+            RemotePort = '443'
+        }
+        xFireWall OMS_Sandbox_Access
+        {
+            Direction = 'Outbound'
+            Name = 'HybridWorker-Sandbox'
+            DisplayName = 'Hybrid Runbook Worker (Sandbox)'
+            Description = 'Allow Hybrid Runbook Worker Communication'
+            Enabled = $true
+            Action = 'Allow'
+            Protocol = 'TCP'
+            RemotePort = '9354'
+        }
+        xFireWall OMS_PortRange
+        {
+            Direction = 'Outbound'
+            Name = 'HybridWorker-PortRange'
+            DisplayName = 'Hybrid Runbook Worker (Port-Range)'
+            Description = 'Allow Hybrid Runbook Worker Communication'
+            Enabled = $true
+            Action = 'Allow'
+            Protocol = 'TCP'
+            RemotePort = '30000-30199'
+        }
+    }
+}
