29cf5620db44a71ec83397d99b855fc2bfa7e640
Ryan Andorfer <ryan.andorfer@gmail.com>
2016-04-21 14:04:31 -0500

    new remediation

---
+++ Runbooks/Remediation/Invoke-UserLogonRemediation.ps1
@@ -0,0 +1,38 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+#>
+Param(
+
+)
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Invoke-UserLogonRemediation
+
+$Vars = Get-BatchAutomationVariable -Prefix 'HelloWorld' -Name 'EmailAccessCredentialName', 'Who'
+$Credential = Get-AutomationPSCredential -Name $Vars.EmailAccessCredentialName
+
+Try
+{
+    $EWSCon = New-EWSMailboxConnection -Credential $Credential
+
+    $Null = Send-EWSEmail -mailboxConnection $EWSCon `
+                          -Recipients 'Ryan.Andorfer@microsoft.com' `
+                          -Subject 'Something Happened!' `
+                          -ImportanceLevel High `
+                          -Body 'Hello'
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters
